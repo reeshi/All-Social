9cdba807936938cf8613d336e975dc80e662b1e7
Rishikesh Yadav <yadavrishikesh53@gmail.com>
2022-03-03 13:34:24 +0530

    setup partial, layouts and static files

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,6 @@
 // home controller
 module.exports.home = function(req, res){
-    return res.end("<h1>Express is up for All Social</h1>");
+    return res.render("home", {
+        title: "Home"
+    });
 }
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const port = 8000;
 
 const app = express();
@@ -6,6 +7,12 @@ const app = express();
 // set up the view engine
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.use(express.static("./assets"));
+
+app.use(expressLayouts);
+// extract the styles and script from sub-pages into the layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // use express router
 app.use("/", require("./routes/index"));
