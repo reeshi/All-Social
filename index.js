8f6d18035c121bfb20e854550574966003340fd3
Rishikesh Yadav <yadavrishikesh53@gmail.com>
2022-03-24 14:40:20 +0530

    add chat engine

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const cookieParser = require("cookie-parser");
+const port = 8000;
+const app = express();
 const db = require("./config/mongoose");
 // used for session cookies
 const session = require("express-session");
@@ -14,8 +16,11 @@ const sassMiddleware = require("node-sass-middleware");
 const flash = require("connect-flash");
 const customMiddleware = require("./config/middleware");
 
-const port = 8000;
-const app = express();
+// set up the chat server to be used with socket.io
+const chatServer = require("http").Server(app);
+const chatSockets = require("./config/chat_sockets").chatSockets(chatServer);
+chatServer.listen(5000);
+console.log("chat server is listening on port 5000");
 
 app.use(sassMiddleware({
     src: "./assets/sass",
